bb09807c866d0df65c6e36ed5090edce20c0e636
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-02-24 14:31:59 +0200

    Make calculator work

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-let firstNumber = "";
-let secondNumber = "";
-let operator = "";
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
+
+const numberButtons = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const operatorButtons = ["+", "-", "*", "/"];
+
+let firstOperand = null;
+let secondOperand = null;
+let operator = null;
+let displayValue = "";
 
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
@@ -21,9 +28,91 @@ const operate = (operator, a, b) => {
   return calculations[operator];
 };
 
-firstNumber = "1";
-secondNumber = "2";
-console.log(operate("+", firstNumber, secondNumber)); // 3
-console.log(operate("-", firstNumber, secondNumber)); // -1
-console.log(operate("*", firstNumber, secondNumber)); // 2
-console.log(operate("/", firstNumber, secondNumber)); // 0.5
+const populateDisplay = (value) => {
+  displayValue += value;
+};
+
+const updateDisplay = (value) => {
+  display.textContent = value;
+};
+
+const resetValues = () => {
+  displayValue = "";
+  firstOperand = null;
+  secondOperand = null;
+  operator = null;
+};
+
+const clearDisplay = () => {
+  console.log("Clear calculator");
+  display.textContent = "0";
+};
+
+const addToOperand = () => {
+  console.log("add to operand");
+  console.log(operator, firstOperand, secondOperand);
+  if (!operator) {
+    firstOperand = displayValue;
+  } else {
+    secondOperand = displayValue;
+  }
+};
+
+const evaluate = () => {
+  const isDivideByZero = divideByZero();
+  console.log(isDivideByZero);
+  if (isDivideByZero) {
+    console.log("Division by zero");
+    displayValue = "LOL, zero divide!";
+    updateDisplay(displayValue);
+    resetValues();
+    return;
+  }
+  const result = operate(operator, firstOperand, secondOperand);
+  updateDisplay(result);
+  firstOperand = result;
+  secondOperand = null;
+  operator = null;
+};
+
+const divideByZero = () => {
+  return (firstOperand == 0 || secondOperand == 0) && operator == "/";
+};
+
+const clickEqual = () => {
+  console.log("click Equal");
+  if (!firstOperand || !secondOperand || !operator) {
+    return;
+  }
+  evaluate();
+};
+
+const clickOperator = (buttonId) => {
+  console.log("click Operator");
+  if (firstOperand && secondOperand && operator) {
+    evaluate();
+  }
+  operator = buttonId;
+  displayValue = "";
+};
+
+buttons.forEach((button) => {
+  if (numberButtons.includes(button.id)) {
+    button.addEventListener("click", () => {
+      populateDisplay(button.id);
+      addToOperand();
+      updateDisplay(displayValue);
+    });
+  } else if (operatorButtons.includes(button.id)) {
+    button.addEventListener("click", () => clickOperator(button.id));
+  } else if (button.id === "clear") {
+    button.addEventListener("click", () => {
+      resetValues();
+      clearDisplay();
+    });
+  } else if (button.id === "=") {
+    button.addEventListener("click", () => {
+      clickEqual();
+    });
+  }
+});
